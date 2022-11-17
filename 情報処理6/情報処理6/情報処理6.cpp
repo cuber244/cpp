4df9ba8ec1ae85932d38d9bab7f3fc27b8db171f
cuber244 <cuber2470@gmail.com>
2022-11-17 13:53:55 +0900

    add alert

--- 情報処理6/情報処理6/情報処理6.cpp
+++ 情報処理6/情報処理6/情報処理6.cpp
@@ -1,25 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
+
+
+void alert()
 {
-   
-
-    
-
-}
-
-
-
-
-
-int sum(int a, int b)
-{
-    return a + b;
+    cout << '\a';
 }
 int main()
 {
-    int wa;
-    wa = sum(10, 50); // int a = 10, b=50; wa = sum(a, b);としてみる 
-    cout << wa << ".\n";
+    alert();
+    alert();
 }
