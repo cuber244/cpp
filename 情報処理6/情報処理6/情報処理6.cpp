fec6e102db0c4a03f769d801c52e4858e0518832
cuber244 <cuber2470@gmail.com>
2022-11-17 13:59:24 +0900

    関数のプロトタイプ宣⾔

--- 情報処理6/情報処理6/情報処理6.cpp
+++ 情報処理6/情報処理6/情報処理6.cpp
@@ -1,17 +1,28 @@
 ﻿#include <iostream>
+#include <random> // 乱数生成のためのライブラリ・ヘッダファイル 
+
 using namespace std;
-
-
-void mypair(int a = 1, int b = 2)
-{
-	cout << "a: " << a << " ";
-	cout << "b: " << b << endl;
+unsigned int get_rand()
+{ // 64ビット幅の乱数を生成する関数 
+	static std::mt19937_64 mt64(0);
+	return mt64();
 }
+
 int main()
 {
-	mypair(3, 4); // 引数全部指定 
-	mypair(5); // 最初だけ指定 
-	mypair(); // 全く指定しない 
+	const int NUM_STUDENTS = 50;
+	int eng[NUM_STUDENTS], math[NUM_STUDENTS];
+	
+		//eng[0] = 90; こういう代入もありますが、面倒なので 
+		//eng[1] = 62; 
+		for (int i = 0; i < NUM_STUDENTS; i++) eng[i] = get_rand() % 101;
+	for (int i = 0; i < NUM_STUDENTS; i++) math[i] = get_rand() % 101;
+	
+		cout << "英語" << endl;
+	for (int i = 0; i < NUM_STUDENTS; i++) cout << i << ": " << eng[i] << endl;
+	cout << "数学" << endl;
+	for (int i = 0; i < NUM_STUDENTS; i++) cout << i << ": " << math[i] << endl;
 	
 }
+
 
