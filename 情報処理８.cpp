f52fb4355fc7fa18a99571bce57ef42be2850316
cuber244 <cuber2470@gmail.com>
2022-12-15 15:23:59 +0900

    test

--- 情報処理８.cpp
+++ 情報処理８.cpp
@@ -13,7 +13,7 @@ private:
 	void show() {
 		std::cout << "名前：" << name << std::endl;
 		std::cout << "年齢：" << age << std::endl;
-		15. std::cout << "番号：" << no << std::endl;
+		std::cout << "番号：" << no << std::endl;
 	}
 };
 int main()
@@ -21,7 +21,7 @@ int main()
 	std::vector<Student> sv;
 	// std::vectorr<Student>::iterator it; // ここで書かないで、以下のようなことも・・・ 
 	sv.push_back(Student("相田みつ", 22, 1));
-	25. sv.push_back(Student("伊藤貞子", 21, 2));
+	sv.push_back(Student("伊藤貞子", 21, 2));
 	sv.push_back(Student("上田祥子", 23, 3));
 	// 自分のデータを入れてみる。 
 	for (std::vector<Student>::iterator it = sv.begin(); it != sv.end(); ++it) // 長いけどよく見るので 30. { 
